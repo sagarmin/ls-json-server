bcc7f2f2b09d0a60face1937f8057f360127d088
Sagar Malla <sagarm.in@mouritech.com>
2023-07-23 16:46:57 +0530

    online proj code

--- server.js
+++ server.js
@@ -2,22 +2,19 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
-
-// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
+	// Add custom route here if needed
+	jsonServer.rewriter({
+		"/api/*": "/$1",
+	})
 );
 server.use(router);
-// Listen to port
 server.listen(3000, () => {
- console.log("JSON Server is running");
+	console.log("JSON Server is running");
 });
 
 // Export the Server API
